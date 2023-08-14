79fa7e1de382a009615c54a6429b5ed552a76e6a
KIM jiyun <138554290+kirnjiyun@users.noreply.github.com>
2023-08-14 21:41:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,14 @@
+function solution(price) {
+    let discount = 0;
+    
+    if (price >= 500000) {
+        discount = 0.2;
+    } else if (price >= 300000) {
+        discount = 0.1;
+    } else if (price >= 100000) {
+        discount = 0.05;
+    }
+    
+    let discountedPrice = price - (price * discount);
+    return parseInt(discountedPrice);
+}
