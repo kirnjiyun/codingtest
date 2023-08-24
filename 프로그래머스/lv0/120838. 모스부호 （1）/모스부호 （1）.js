d0539ed231d32c7c207fc129efd09c0c011c67f1
KIM jiyun <138554290+kirnjiyun@users.noreply.github.com>
2023-08-24 15:52:14 +0900

    [level 0] Title: 모스부호 (1), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    var morse = { 
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+        '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+        '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+        '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+        '-.--': 'y', '--..': 'z'
+    };
+
+    var morseCodeList = letter.split(' ');
+
+    var decodedMessage = morseCodeList.map(function(code) {
+        return morse[code];
+    });
+
+    return decodedMessage.join('');
+}
