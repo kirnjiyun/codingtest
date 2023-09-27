37b6d4ae55704f8939627f3ad2d0dbf03d3596c6
KIM jiyun <138554290+kirnjiyun@users.noreply.github.com>
2023-09-27 11:10:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.10 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0 || arr[i] !== arr[i - 1]) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
