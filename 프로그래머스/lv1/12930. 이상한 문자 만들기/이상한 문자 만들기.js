ce10bec763372109e6852792a1e9a3ec2f453529
KIM jiyun <138554290+kirnjiyun@users.noreply.github.com>
2023-10-02 22:31:47 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+  let answer =s.split(" ")
+    .map((x) =>
+       x.split("").map((y, index) => (index % 2 == 0 ? y.toUpperCase() : y.toLowerCase())).join("")).join(" ");
+    
+    return answer;
+}
