d10eb972d968446a25037ace936927773bbe1c74
KIM jiyun <138554290+kirnjiyun@users.noreply.github.com>
2023-10-14 15:37:08 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.76 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    const lastSeen = {};  
+    const result = [];    
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (lastSeen[char] !== undefined) {
+            result.push(i - lastSeen[char]);
+        } else {result.push(-1);
+        }
+       
+        lastSeen[char] = i;
+    }
+
+    return result;
+}
